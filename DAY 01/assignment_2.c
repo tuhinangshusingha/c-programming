e4c943505a389cea652920f70186b3f6ebe675bf
tuhinangshusingha <tuhinangshusingha@gmail.com>
2025-08-19 16:16:02 +0530

    Add files via upload

---
+++ DAY 01/assignment_2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main () {
+
+    float a,b,c ;
+    printf ("Enter two numbers: ") ;
+    scanf ("%f %f",&a,&b) ;
+    c=a+b ;
+    printf ("sum is : %f" ,c) ;
+    return 0 ;
+}
